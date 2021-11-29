eedb0cc682d080baf158859978b9653105d44e9e
piotrekmroz28 <piotrekmroz10@gmail.com>
2021-11-29 20:23:55 +0100

    Zakonczona aplikacja Angular lista cytatow

--- Angular/best-quotes-app-master/src/app/app.module.ts
+++ Angular/best-quotes-app-master/src/app/app.module.ts
@@ -2,10 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RankingComponent } from './ranking/ranking.component';
+import { FormComponent } from './form/form.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    RankingComponent,
+    FormComponent,
+    ButtonComponent,
+  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
