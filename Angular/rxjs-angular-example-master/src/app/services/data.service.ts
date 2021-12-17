f4784ddcdd6b4ec03bb5e4c182b3c5d314967fcf
piotrekmroz28 <piotrekmroz10@gmail.com>
2021-12-17 10:30:17 +0100

    Rozpoczecie pracy nad kolejna aplikacja Angular

--- Angular/rxjs-angular-example-master/src/app/services/data.service.ts
+++ Angular/rxjs-angular-example-master/src/app/services/data.service.ts
@@ -1,10 +1,36 @@
 import { Injectable } from '@angular/core';
 import { DataBaseService, Person } from './data-base.service';
 import { BehaviorSubject, Observer, PartialObserver, Subject } from 'rxjs';
+import { threadId } from 'worker_threads';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
 
+  behaviousSubject = new BehaviorSubject<Person[]>([]);
+  subject = new Subject<Person[]>();
+
+  constructor(private dataBaseService: DataBaseService) {
+    this.init();
+  }
+
+  addPerson(person: Person){
+    this.dataBaseService.addPerson(person).subscribe(this.observer());
+  }
+
+  private init(): void{
+    this.dataBaseService.fetchPersons().subscribe(this.observer())
+  }
+
+  private observer(): Observer<Person[]> {
+    return {
+      next: (persons: Person[]) => {
+        this.behaviousSubject.next(persons);
+        this.subject.next(persons);
+      },
+      error: error => console.error(error),
+      complete: () => console.log('Completed')
+    }
+  }
 }
